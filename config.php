0dd8bb0e2384e89116d9fdaff1c9c930001c180e
didinkaj <didinkaj@gmail.com>
2019-06-27 23:48:47 +0300

    Update config.php

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ return [
         'name' => 'd2h1mk1nkbhejg',
         'username' => 'jialcvwpepcxyn',
         'password' => '27117339c7c239f87df5d7d4bd74b44437431de8f1ebe30454dbd7229f89c3b9',
-        'connection' => 'mysql:host=ec2-174-129-227-128.compute-1.amazonaws.com',
+        'connection' => 'pgsql:host=ec2-174-129-227-128.compute-1.amazonaws.com',
         'port' => '5432',
         'options' => [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
